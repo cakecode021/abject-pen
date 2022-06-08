d1ab813e6fc58956703545f0abe7bc8f0f63a239
cakecode <105256041+cakecode021@users.noreply.github.com>
2022-06-09 01:19:05 +0900

    I add a small change

--- abject-pen/abject-pen.cpp
+++ abject-pen/abject-pen.cpp
@@ -23,20 +23,20 @@ using namespace std;
 
 int main() {
 
-  int first_number, second_number, sum;
+    int first_number, second_number, sum;
     
-  cout << "Enter two integers: ";
-  cin >> first_number >> second_number;
+    cout << "Enter two integers: ";
+    cin >> first_number >> second_number;
 
-  sum = first_number + second_number;
+    sum = first_number + second_number;
 
   // prints sum 
-  cout << first_number << " + " <<  second_number << " = " << sum;     
+    cout << first_number << " + " <<  second_number << " = " << sum;     
 
-  return 0;
+    return 0;
 }*/
 
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 
 int main()
@@ -52,6 +52,30 @@ int main()
     quotient = dividend / divisor;
 
     cout << "Quotient = " << quotient << endl;
+
+    return 0;
+}*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{    
+    int durable life, compensation rate, price obtained;
+
+    cout << "Enter durable life: ";
+    cin >> durable life;
+
+    cout << "Enter compensation rate: ";
+    cin >> compensation rate;
+
+    amortization in 1st year =  price obtained * compensation rate / 100;
+    amortization amount for each year starting from the 2nd year =  price obtained * compensation rate / 100;
+    amortization in the final year =  price obtained * compensation rate / 100 - 1;
+
+    cout << "amortization in 1st year = " << amortization in 1st year << endl;
+    cout << "amortization amount for each year starting from the 2nd year = " << amortization amount for each year starting from the 2nd year << endl;
+    cout << "amortization in the final year = " << amortization in the final year << endl;
 
     return 0;
 }
